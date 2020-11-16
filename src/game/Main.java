bdc421b42b39650627b0f528c4db225ee43f81ca
rochelle-m <mca.1930@unigoa.ac.in>
2020-11-16 19:17:49 +0530

    Add initial project files

---
+++ src/game/Main.java
@@ -0,0 +1,23 @@
+package game;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static final String APP_NAME = "Monopoly";
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("layout.fxml"));
+        primaryStage.setTitle(APP_NAME);
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
