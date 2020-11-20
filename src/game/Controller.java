0a9b66e1ce2e07e40d812cea655b7d38e0dceebb
rochelle-m <mca.1930@unigoa.ac.in>
2020-11-20 16:48:20 +0530

    Add a spinner and a button

--- src/game/Controller.java
+++ src/game/Controller.java
@@ -1,4 +1,16 @@
 package game;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Spinner;
+
 public class Controller {
+    public Button btnPlay;
+    public Spinner spinner;
+    public Label title;
+
+    public void play() {
+        System.out.println(spinner.getValue());
+    }
+
 }
