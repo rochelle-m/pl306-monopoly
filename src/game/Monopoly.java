b95e413ea08521486a1f74264d205ae642eb3d76
rochelle-m <mca.1930@unigoa.ac.in>
2020-11-27 21:00:31 +0530

    Add Monopoly class

--- src/game/Monopoly.java
+++ src/game/Monopoly.java
@@ -6,12 +6,26 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
 public class Monopoly extends Application {
+    List<Player> players;
     private Integer numOfPlayers;
 
     public Monopoly(Integer numOfPlayers) {
+        Integer[] ids = {1, 2, 3, 4};
+        String[] names = {"A", "B", "C", "D"};
+
         this.numOfPlayers = numOfPlayers;
-        System.out.println("Game on "+ numOfPlayers);
+        players = new ArrayList<>();
+        IntStream.range(0, numOfPlayers).mapToObj(i -> new Player(ids[i], names[i], (float) 1000.00, false)).
+                forEachOrdered(p -> players.add(p));
+    }
+
+    void removePlayer(){
+        players.removeIf(p -> p.getAmountOfMoney() == (float) 0.0);
     }
 
     public Integer getNumOfPlayers() {
