e6ea0600cd6888e132e4c0385baba24db642da13
chetana-1929 <mca.1929@unigoa.ac.in>
2020-11-29 11:33:05 +0530

    add bank

---
+++ src/game/Bank.java
@@ -0,0 +1,43 @@
+package game;
+
+public class Bank {
+    private int bankMoney;
+    final static int PAYMENT_FOR_JAIL = 1000;
+    final int MAXIMUM_HOUSE_NUMBER = 24;
+    final int MAXIMUM_HOTEL_NUMBER = 12;
+    // public ArrayList<PropertyCard> propertyCardList;
+    // private ArrayList<ChanceCard> chanceCardList;
+    // private ArrayList<CommunityChestCard> communityChestCardList;
+
+    public Bank() {
+        this.bankMoney =  0;
+    }
+    public Bank(int bankMoney) {
+        this.bankMoney = bankMoney;
+    }
+
+    public void giveMoneyToPlayer(Player player, int amount) {
+        player.setAmountOfMoney(amount);
+        this.bankMoney -= amount;
+    }
+
+    public int getBankMoney() {
+        return bankMoney;
+    }
+
+    public void setBankMoney(int bankMoney) {
+        this.bankMoney = bankMoney;
+    }
+
+    public static int getPaymentForJail() {
+        return PAYMENT_FOR_JAIL;
+    }
+
+    public int getMAXIMUM_HOUSE_NUMBER() {
+        return MAXIMUM_HOUSE_NUMBER;
+    }
+
+    public int getMAXIMUM_HOTEL_NUMBER() {
+        return MAXIMUM_HOTEL_NUMBER;
+    }
+}
