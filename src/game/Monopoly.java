ad6d601214bab5efda1dfa8a2323529ca30afb85
rochelle-m <mca.1930@unigoa.ac.in>
2020-12-02 23:25:57 +0530

    Set players

--- src/game/Monopoly.java
+++ src/game/Monopoly.java
@@ -2,10 +2,11 @@ package game;
 
 import javafx.fxml.Initializable;
 import javafx.scene.layout.GridPane;
-
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.stream.IntStream;
 
 public class Monopoly implements Initializable {
     public GridPane board;
@@ -16,8 +17,13 @@ public class Monopoly implements Initializable {
         return players;
     }
 
-    public void setPlayers(List<Player> players) {
-        this.players = players;
+    public void setPlayers() {
+        Integer[] ids = {1, 2, 3, 4};
+        String[] names = {"A", "B", "C", "D"};
+        this.players = new ArrayList<>();
+        System.out.println(this.numOfPlayers);
+        int bound = numOfPlayers;
+        IntStream.range(0, bound).mapToObj(i -> new Player(ids[i], names[i], (float) 1000.00, false)).forEach(player -> players.add(player));
     }
 
     public Integer getNumOfPlayers() {
@@ -26,6 +32,7 @@ public class Monopoly implements Initializable {
 
     public void setNumOfPlayers(Integer numOfPlayers) {
         this.numOfPlayers = numOfPlayers;
+        setPlayers();
     }
 
     @Override
