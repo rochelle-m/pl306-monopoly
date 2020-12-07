f8030a9af9a9ef604318d53d9678104d0f1ba12f
rochelle-m <mca.1930@unigoa.ac.in>
2020-12-08 00:54:03 +0530

    Changes
    Fix color, add board bgcolor

--- src/game/Square.java
+++ src/game/Square.java
@@ -1,5 +1,6 @@
 package game;
 
+import javafx.scene.Node;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
@@ -19,12 +20,16 @@ public class Square {
     // trial
     public void addPlayerToSquare(Player player){
         Circle c = new Circle();
-//        c.setStroke(Color.BLACK);
+        String color = player.getTokenColor();
+        int r = Integer.parseInt(color.substring(1,3),16);
+        int g = Integer.parseInt(color.substring(3,5),16);
+        int b = Integer.parseInt(color.substring(5,7),16);
+        c.setFill(Color.rgb(r, g, b));
+        c.setId(player.getName());
         c.setRadius(8.0);
         c.setLayoutX(player.getId() * 18.0);
         c.setLayoutY(24.0);
-        c.setStyle("-fx-background-color :"+player.getTokenColor()+";");
-        this.PANE.getChildren().add(c);
+        this.PANE.getChildren().add(player.getId(), c);
     }
 
     public String getSQUARE_NAME() {
